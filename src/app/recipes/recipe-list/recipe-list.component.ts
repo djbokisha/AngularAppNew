8fb12eecd4381c4409c6e4d73f119cad396d7a7b
djbokisha <bojan.bokisha@gmail.com>
2022-02-04 01:46:52 +0100

    recipe commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,9 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe [] = [
     new Recipe ("A Test Recipe", "This is simply a test","https://images.themodernproper.com/billowy-turkey/production/posts/2021/Grilled-Sweet-Potatoes-9.jpeg?w=1280&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1628395628&s=53f098edd3dad2dc0d949446f026bfb1"),
     new Recipe ("A Test Recipe 2", "This is simply a test","https://images.themodernproper.com/billowy-turkey/production/posts/2021/Grilled-Sweet-Potatoes-9.jpeg?w=1280&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1628395628&s=53f098edd3dad2dc0d949446f026bfb1"),
-    new Recipe ("A Test Recipe 3", "This is simply a test","https://images.themodernproper.com/billowy-turkey/production/posts/2021/Grilled-Sweet-Potatoes-9.jpeg?w=1280&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1628395628&s=53f098edd3dad2dc0d949446f026bfb1")
+    new Recipe ("A Test Recipe 3", "This is simply a test","https://images.themodernproper.com/billowy-turkey/production/posts/2021/Grilled-Sweet-Potatoes-9.jpeg?w=1280&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1628395628&s=53f098edd3dad2dc0d949446f026bfb1"),
+    new Recipe ("A Test Recipe 4", "This is simply a test","https://images.themodernproper.com/billowy-turkey/production/posts/2021/Grilled-Sweet-Potatoes-9.jpeg?w=1280&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1628395628&s=53f098edd3dad2dc0d949446f026bfb1")
+
 
 
   ];
